a17bf95be934be3d092c472d33258b6c6d1cc846
JoaoGalante16 <164560587+JoaoGalante16@users.noreply.github.com>
2025-11-08 18:47:03 -0300

    details base de conhecimento

---
+++ SolvITSupport/Migrations/20251108212437_AddHelpfulCounts.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SolvITSupport.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHelpfulCounts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "HelpfulCount",
+                table: "BaseConhecimento",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "NotHelpfulCount",
+                table: "BaseConhecimento",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HelpfulCount",
+                table: "BaseConhecimento");
+
+            migrationBuilder.DropColumn(
+                name: "NotHelpfulCount",
+                table: "BaseConhecimento");
+        }
+    }
+}
